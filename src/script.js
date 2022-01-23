ae57f8a13320f07b009b5af416f31dfb91cdfe8b
RogueArt <altoraries@gmail.com>
2022-01-22 19:47:10 -0800

    Refactor code to use navigator, get links only when complete

--- src/script.js
+++ src/script.js
@@ -1,50 +1,19 @@
-import { filterRelatedQuestionLinks, getAllTopLevelLinks, setFocus, resetFocus } from './utils.js'
+import { LinksNavigator } from './navigator.js'
 
-let links = filterRelatedQuestionLinks(getAllTopLevelLinks())
+// Monitor document as new content loads in
+document.addEventListener('readystatechange', (event) => {
+  // Wait until document is complete
+  if (document.readyState !== 'complete') return
 
-let index = 0
-setFocus(links[0])
+  const navigator = new LinksNavigator() 
+  document.addEventListener('keydown', async event => {
+    const { key } = event
 
-// Check if focused on search bar
-function focusedOnSearchBar() {
-  return document.activeElement.tagName === 'INPUT'
-}
+    // Don't do anything if on search bar
+    if (navigator.isFocusedOnSearchBar()) return
 
-document.addEventListener('keydown', async event => {
-  const { key } = event
-
-  // Don't do anything if on search bar
-  if (focusedOnSearchBar()) return
-
-  // Make sure we can access all 10 questions
-  if (links.length < 10) {
-    links = filterRelatedQuestionLinks(getAllTopLevelLinks())
-  }
-
-  // Go to link above
-  if (key === 'k') {
-    if (index === 0) return
-
-    // Reset style of current
-    resetFocus(links[index])
-    setFocus(links[index - 1])
-
-    // Decrease index by one
-    index -= 1
-
-    // Scroll to top if hit first link
-    if (index === 0) window.scrollTo(0, 0)
-  }
-
-  // Go to link below
-  if (key === 'j') {
-    if (index === links.length - 1) return
-
-    resetFocus(links[index])
-    setFocus(links[index + 1])
-    index += 1
-
-    // Scroll to bottom if hit first link
-    // if (index === links.length - 1) window.scrollTo(0, document.body.scrollHeight)
-  }
+    // Navigate based on key
+    if (key === 'k') navigator.goToLinkAbove()
+    if (key === 'j') navigator.goToLinkBelow()
+  })
 })
