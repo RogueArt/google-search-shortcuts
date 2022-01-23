3df442233a63fa9422743b3ec7608a5aab542d35
RogueArt <altoraries@gmail.com>
2022-01-22 17:46:14 -0800

    Configure to build a zippped version of code automatically

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const ZipPlugin = require('zip-webpack-plugin')
 
 module.exports = {
   mode: 'production', // "production" | "development" | "none"
   entry: './src/script.js',
   output: {
     // options related to how webpack emits results
-    path: path.resolve(__dirname, 'dist'), // string (default)
+    path: path.resolve(__dirname, 'dist/src'), // string (default)
     // the target directory for all output files
     // must be an absolute path (use the Node.js path module)
     filename: 'script.js', // string (default)
@@ -15,8 +16,14 @@ module.exports = {
     // the url to the output directory resolved relative to the HTML page
   },
   plugins: [
+    // Copy manifest.json to dist folder
     new CopyWebpackPlugin({
       patterns: [path.resolve(__dirname, './src/manifest.json')],
     }),
+    // Zip the output
+    new ZipPlugin({
+      path: path.resolve(__dirname, 'dist/zip'),
+      filename: 'google-shortcuts-extension.zip',
+    })
   ],
 }
